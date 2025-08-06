f6ad91af71f6feedba8f1d57fc785d63a6b6a411
Amvina Pham <215578691+AmvP100@users.noreply.github.com>
2025-08-06 16:19:44 -0700

    code updated

---
+++ shoppingcart.js
@@ -0,0 +1,44 @@
+const storeProducts = {
+  apples: 1.5,
+  bread: 2.0,
+  milk: 2.5,
+  eggs: 3.0,
+  cheese: 4.0
+};
+
+const storeItemsDiv = document.getElementById("store-items");
+
+const cartList = document.getElementById("cart");
+const cartTotalSpan = document.getElementById("cart-total");
+
+for (const item in storeProducts) {
+  const price = storeProducts[item];
+
+  const itemButton = document.createElement("button");
+
+  const buttonText = document.createTextNode(item + " - $" + price.toFixed(2));
+
+  itemButton.appendChild(buttonText);
+
+  itemButton.addEventListener("click", function(event) {
+    addToCart(item, price);
+  });
+
+  storeItemsDiv.appendChild(itemButton);
+}
+
+let total = 0;
+
+function addToCart(item, price) {
+  const list = document.createElement("li");
+
+  const itemText = document.createTextNode(item + " - $" + price.toFixed(2));
+
+  list.appendChild(itemText);
+
+  cartList.appendChild(list);
+
+  total = total + price;
+
+  cartTotalSpan.innerHTML = total.toFixed(2);
+}
